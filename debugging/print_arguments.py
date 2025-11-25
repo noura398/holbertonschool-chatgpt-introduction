5493b609366449057c63a63b8c8b3352d97f2dbe
noura398 <0nourasubaie@gmail.com>
2025-11-25 11:48:12 +0000

    a

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,13 +1,7 @@
 #!/usr/bin/python3
 import sys
 
-def factorial(n):
-    result = 1
-    while n > 1:
-        result *= n
-        n -= 1
-    return result
-
-f = factorial(int(sys.argv[1]))
-print(f)
+if __name__ == "__main__":
+    for arg in sys.argv[1:]:
+        print(arg)
 
